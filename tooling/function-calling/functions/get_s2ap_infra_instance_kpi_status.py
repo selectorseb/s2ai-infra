225013701d23e2c6b41d5751093ac8654f6103dc
Aniket Gawade <aniket.mailbot@gmail.com>
2024-06-21 16:13:28 -0700

    Add kpi infra status

--- tooling/function-calling/functions/get_s2ap_infra_instance_kpi_status.py
+++ tooling/function-calling/functions/get_s2ap_infra_instance_kpi_status.py
@@ -3,25 +3,23 @@ import requests
 from time import strftime, localtime
 
 
-def get_s2ap_infra_health_status(role: str = 'all', s2_inst: str = 'all', time: str = "last 30 min") -> str:
+def get_s2ap_infra_instance_kpi_status(role: str = 'all', s2_inst: str = 'all', kpi_name: str = 'all', time: str = "last 30 min") -> str:
     """
-    Function to get status of s2ap infra health
+    Function to get status of s2ap infra instance kpi state
 
     Parameters:
     - role (str): The role, e.g., 'prod', 'staging' or 'poc'.
     - s2_inst (str): The instance name.
+    - kpi_name (str): The kpi name, e.g., 'bist' or 'pv'. Default is 'all'.
     - time (str): The time range, e.g., 'last 1 day', 'last 30 minutes' or 'last 2 days'. Default is 'last 30 min'.
 
     Returns:
-    - str: Returns the state of the s2ap infra health status or an error message.
+    - str: Returns status of s2ap infra instance kpi state.
     """
 
     selector_url = "https://s2m.selector.ai/api/collab2-slack/rules/command"
     api_key = "70fN07ny3x3sENNwEO2o8cJr"
-    query_to_run = '#select s2ap_infra_health as honeycomb show-by s2_inst, role where '
-    query_to_run = query_to_run + ' s2_inst=.*, '
-    query_to_run = query_to_run + 'role=.*, '
-    query_to_run = query_to_run + 's2ap_infra_health>=1, '
+    query_to_run = '#select s2ap_infra_health_by_kpi as honeycomb show-by role, s2_inst, kpi_name'
 
     query_to_run = query_to_run + ' for ' + time 
 
@@ -39,7 +37,7 @@ def get_s2ap_infra_health_status(role: str = 'all', s2_inst: str = 'all', time: 
     
     if len(result['data']) == 0:
         return "No result returned."
-    output = "Following s2ap infra health status reported: \n"
+    output = "Following s2ap kpi health status reported: \n"
     for item in (result['data']):
         if s2_inst != 'all' and item['s2_inst'] != s2_inst:
             continue
@@ -47,16 +45,20 @@ def get_s2ap_infra_health_status(role: str = 'all', s2_inst: str = 'all', time: 
         if role != 'all' and item['role'] != role:
             continue
 
+        if kpi_name != 'all' and item['kpi_name'] != kpi_name:
+            continue
+
         entry = " - instance:" + item['s2_inst']
         entry = entry + " role:" + item['role']
+        entry = entry + " kpi:" + item['kpi_name']
         # if item['s2ap_infra_health'] is 1 then return healthy else return unhealthy
-        if item['s2ap_infra_health'] == 1:
-            entry = entry + " health:good"
+        if item['s2ap_infra_health_by_kpi'] >= 1:
+            entry = entry + " kpi health:violating"
         else:
-            entry = entry + " health:violating"
+            entry = entry + " kpi health:good"
         entry = entry + " \n"
         output = output + entry
     return output
 
 # Test function
-# print(get_s2ap_infra_health_status())
+# print(get_s2ap_infra_instance_kpi_status())
